2761c28bfaf22b8da61a04017d02efe39dd9e751
DorothyAriana <arianadottie@gmail.com>
2023-07-01 19:32:02 +0300

    completed assignment

---
+++ server/migrations/versions/c9208f26ab0a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c9208f26ab0a
+Revises: c93a14d1aa19
+Create Date: 2023-07-01 17:30:42.913976
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c9208f26ab0a'
+down_revision = 'c93a14d1aa19'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
